a099f69887d1a7b65a4d16295195dc7de80fb45a
Alexander <alexander.nakahara@ludospro.com.br>
2020-01-19 13:52:11 -0300

    Changing Session Storage to Local Storage

--- User Management/controllers/UserController.js
+++ User Management/controllers/UserController.js
@@ -196,9 +196,9 @@ class UserController {
 
         let users = [];
 
-        if (sessionStorage.getItem("users")) {
+        if (localStorage.getItem("users")) {
 
-            users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
         }
 
         return users;
@@ -221,10 +221,11 @@ class UserController {
         let users = this.getUsersStorage();
 
         users.push(data);
-        sessionStorage.setItem("users", JSON.stringify(users));
+        // sessionStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("users", JSON.stringify(users));
 
     }
-    
+
     addLine(dataUser) {
 
         let tr = document.createElement("tr");
